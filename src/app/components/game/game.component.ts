138f47c533baf72d7d75ce5d7f57e9008c87bb61
Ege Durukan <egedurukan@gmail.com>
2019-10-26 11:22:22 +0200

    fix(ID-2E): everything fixed. Its been a rewarding game but still a game

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -32,15 +32,15 @@ export class GameComponent implements OnInit {
   timesArr;
   trackCount: number = 0;
   score: number = 0;
-  
+
   private ctx: CanvasRenderingContext2D;
 
   constructor(private musicDataService: MusicdataService) { }
 
   ngOnInit(): void {
     this.ctx = this.canvas.nativeElement.getContext('2d');
-    this.getMusicData();
-    this.musicDataService.fetchMusicData().subscribe(data =>{ this.moderateResponse(data) });
+  
+    this.musicDataService.fetchMusicData().subscribe(data =>{ this.moderateResponse(data); this.drawBarsPerBeats(data) });
   }
 
   drawBars(xCoord: number) {
@@ -51,7 +51,8 @@ export class GameComponent implements OnInit {
 
   animate() {
     this.playAudio();
-    // this.ctx.strokeStyle = 'red';
+    this.stopwatch();
+  
     const line = new Line(this.ctx);
     line.move(200, 1);
   }
@@ -83,26 +84,27 @@ export class GameComponent implements OnInit {
       ;
   }
   stopwatch(): void {
-    this.playable = true; // playable is going to used to enable to click functinality(not implemented yet) only if the stopwatch started
+    this.playable = true;
       const startTime = Date.now() - (this.counter || 0);
       this.timerRef = setInterval(() => {
+
         this.counter = Date.now() - startTime;
-        if (this.counter > this.timesArr[this.trackCount]*1.2) {
-          console.log(this.timesArr[this.trackCount]*1.2, this.counter);
-          // in this case, click didn't happened within the 20% of the current track's range therefore trackCount increased
+        if (this.counter > this.timesArr[this.trackCount]+300) {
+          console.log(this.timesArr[this.trackCount]+300, this.counter);
+        
           this.trackCount++;
         }
         if (this.counter >= this.timesArr[this.timesArr.length-1]) { 
-        //this if~else statement is inside of the interval to be able to stop itself automatically.
-        // it is checking the last time of the beat/bar so game can be still playable
+      
+      
         this.playable = false;
         clearInterval(this.timerRef);
         }
       });
   }
   moderateResponse(data:MusicDataResponse): void{ 
-    // this function is taking all "time"s and puts them in an array. Therefore we don't need to worry about how many elements in each section
-    // since just need the time. At the end, we are treating the JSON file as a 1D array, this function will make our work easier
+  
+  
     let singleArr = [];
     for (let i = 0; i < data.tracks.length; i++) {
       for (let j = 0; j < data.tracks[i].length; j++) {
@@ -113,9 +115,9 @@ export class GameComponent implements OnInit {
   }
   clickAction() {
     if (this.playable) {
-      if (this.counter > this.timesArr[this.trackCount]*0.3 && this.counter < this.timesArr[this.trackCount]*1.7) {
-        console.log(this.counter, ' => ', this.timesArr[this.trackCount])
-        if (this.counter > this.timesArr[this.trackCount]*0.9 && this.counter < this.timesArr[this.trackCount]*1.1) {
+      if (this.counter > this.timesArr[this.trackCount]-300 && this.counter < this.timesArr[this.trackCount]+300) {
+      
+        if (this.counter > this.timesArr[this.trackCount]-220 && this.counter < this.timesArr[this.trackCount]+220) {
           console.log(`score: ${this.score} +100`)
           this.score += 100;
           return;
